7e2f2d30c45ca6d1d9a53d3f59081759a0e6332e
smy20 <152443781+smy20@users.noreply.github.com>
2024-07-03 22:48:22 +0300

    Add files via upload

---
+++ pr1.web/JavaScript.js
@@ -0,0 +1,14 @@
+alert('hello')
+
+
+
+
+function ChangeBackgroundColor(){
+    document.getElementById('demo').style.backgroundColor='antiquewhite';
+}
+
+
+
+function t(){
+    document.getElementById('time').innerHTML = Date();
+}
